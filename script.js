b2ef590895ad3e61fc75e2427f9681e75f035201
Herbeton Rocha <61540563+HERBETON25@users.noreply.github.com>
2024-11-23 14:36:13 +0000

    texte 2

--- script.js
+++ script.js
@@ -2,25 +2,13 @@
 document.querySelectorAll('.video-sidebar ul li').forEach(item => {
     item.addEventListener('click', function() {
         const videoSrc = this.getAttribute('data-video');
+        const videoIf = document.getElementById('videoiframe')
         const video = document.getElementById('video');
         const videoSource = document.getElementById('videoSource');
 
-        videoSource.setAttribute('src', videoSrc);
+        videoSource.setAttribute('src', videoIf);
         video.load();
     });
 });
 
-// Lógica para a seção de comentários
-document.getElementById('commentForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const commentInput = document.getElementById('comment');
-    const commentText = commentInput.value;
 
-    if (commentText.trim() !== '') {
-        const commentsList = document.getElementById('commentsList');
-        const newComment = document.createElement('li');
-        newComment.textContent = commentText;
-        commentsList.appendChild(newComment);
-        commentInput.value = '';
-    }
-});
