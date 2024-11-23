e568c242eacd60747c0b486b013fe1de3a58fc0e
Herbeton Rocha <61540563+HERBETON25@users.noreply.github.com>
2024-11-23 15:37:38 -0300

    projeto-quase-pronto

--- script.js
+++ script.js
@@ -2,11 +2,9 @@
 document.querySelectorAll('.video-sidebar ul li').forEach(item => {
     item.addEventListener('click', function() {
         const videoSrc = this.getAttribute('data-video');
-        const videoIf = this.getAttribute('data-video')
         const video = document.getElementById('video');
         const videoSource = document.getElementById('videoSource');
-        const videoiframe = document.getElementById('videoiframe');
-        videoiframe.setAttribute('src', videoIf);
+        videoSource.setAttribute('src',videoSrc)
         video.load();
     });
 });
