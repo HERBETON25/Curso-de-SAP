4a50b5bb1db835b2da4b8f6fe709e5ddc7622eef
Herbeton Rocha <61540563+HERBETON25@users.noreply.github.com>
2024-11-24 13:43:13 -0300

    iframeJavaScript

--- script.js
+++ script.js
@@ -2,9 +2,16 @@
 document.querySelectorAll('.video-sidebar ul li').forEach(item => {
     item.addEventListener('click', function() {
         const videoSrc = this.getAttribute('data-video');
+        const videoIf = this.getAttribute('data-video');
+
         const video = document.getElementById('video');
+
         const videoSource = document.getElementById('videoSource');
-        videoSource.setAttribute('src',videoSrc)
+        const videoIframe = document.getElementById('videoIframe');
+
+        videoSource.setAttribute('src',videoSrc);
+        videoIframe.setAttribute('src', videoIf);
+        
         video.load();
     });
 });
